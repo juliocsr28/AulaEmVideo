1a655b5650e543c819ce89551342a1cf749389f2
juliocsr28 <julioc.ferreira@outlook.pt>
2025-02-14 16:16:57 -0300

    Atividades

--- Aula_14ex/ex_017/script.js
+++ Aula_14ex/ex_017/script.js
@@ -1,1 +1,18 @@
-alert('olá')
+function gerartabuada() {
+    var numero = document.getElementById('txtn')
+    var res = document.getElementById('res')
+    if(numero.value.length == '') {
+        alert('[ERRO] Verifique os números')
+    } else {
+        var n = Number(numero.value)
+        res.innerHTML = ''
+        for(c=1; c<=10; c++) {
+          /*res.innerHTML += `${n} X ${c} = ${n*c}<br> `;*/
+          let item = document.createElement('option')
+          item.text = `${n} X ${c} = ${n*c}`
+          item.value = `res${c}`
+          res.appendChild(item)
+        }    
+    }
+}
+
